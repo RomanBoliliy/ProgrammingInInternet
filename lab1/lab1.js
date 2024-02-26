b3c6b9b91b9f3dab60c5d04e45b34eac8ab1056f
RomanBoliliy <21fi.r.bolilyi@std.npu.edu.ua>
2024-02-26 20:35:13 +0200

    ignore

--- lab1/lab1.js
+++ lab1/lab1.js
@@ -34,22 +34,22 @@ switch (day) {
     case "1":
         alert("Sunday");
         break;
-    case 2:
+    case "2":
         alert("Monday");
         break;
-    case 3:
+    case "3":
         alert("Tuesday");
         break;
-    case 4:
+    case "4":
         alert("Wednesday");
         break;
-    case 5:
+    case "5":
         alert("Thursday");
         break;
-    case 6:
+    case "6":
         alert("Friday");
         break;
-    case 7:
+    case "7":
         alert("saturday");
         break;
 }
